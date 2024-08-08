020136c8bc93b38969ce2106bfb9a94fe645ae76
HiKKors <nk041496@gmail.com>
2024-08-08 23:20:36 +0400

    переход на PostgreSql

--- AccTrackGuides/settings.py
+++ AccTrackGuides/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'AccTrackGuides.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'acc_project',
+        'USER': 'django_admin',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
