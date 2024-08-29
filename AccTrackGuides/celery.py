1053f5bcc8d3f51544606fe6ecde83def9b056da
HiKKors <nk041496@gmail.com>
2024-08-29 13:33:44 +0400

    настройка celery

---
+++ AccTrackGuides/celery.py
@@ -0,0 +1,16 @@
+import os
+
+from celery import Celery
+
+# Установка модуля настроек Django по умолчанию для программы "celery".
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AccTrackGuides.settings')
+
+app = Celery('AccTrackGuides')
+
+# Использование строки здесь означает, что работнику не нужно сериализовать объект конфигурации для дочерних процессов. 
+# - namespace='CELERY' означает, что все клавиши конфигурации, 
+# связанные с сельдереем, должны иметь префикс `CELERY_`.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Загрузка модулей задач из всех зарегистрированных приложений Django.
+app.autodiscover_tasks()
